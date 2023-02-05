e61b112d8ef78bb1f61df6544abfabe007a1d420
Alizsa <124475723+Alizsa@users.noreply.github.com>
2023-02-05 17:52:15 -0500

    Update cleaner.py
    The stopwords portion is functional. Some stopwords were not being omitted because of punctuation and the work around I found for that seems to have removed punctuation altogether. I'm not sure if that's okay..
    
    Attempted to stem, but random words are being cut off. November is being changed to Novemb, not sure how to fix it.

--- cleaner.py
+++ cleaner.py
@@ -2,6 +2,8 @@ import contractions
 import csv
 import re
 import demoji
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer, PorterStemmer
 
 def remove_new_lines(value):
     return ''.join(value.splitlines())
@@ -23,6 +25,16 @@ with open('tweets.csv', encoding='utf-8') as csv_file:
             item = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', 'xxhyperlink', item)     #removes hyperlinks
             item = demoji.replace(item, "")      #removes emojis
             item = re.sub(r'@[A-Za-z0-9_]+', 'xxhandle', item)     #removes @handles
+            stops = set(stopwords.words('english'))  # assign a set of stopwords to a set called stops
+            custom_stopwords = {"xxhandle", "xxhyperlink", "get", "here", "since"}  # set of custom stopwords
+            stops.update(custom_stopwords)  # combine set of stopwords with custom stopwords
+            words = re.findall(r'\b\w+\b', item)  # split text into individual words based on word boundaries
+            words = [word for word in words if word.lower() not in stops]  # remove stopwords
+            stemmer = PorterStemmer()
+            lemmatizer = WordNetLemmatizer()
+            words = [lemmatizer.lemmatize(word) if lemmatizer.lemmatize(word).endswith('e') else stemmer.stem(word) for
+                     word in words]  # stem & lemmatize words
+            item = " ".join(words)  # join words back into sentence
             new_row.append(item)
         print(new_row)
         many.append(new_row)
